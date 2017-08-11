3916d458df15b625e2a2ba03029a02666f85e41a
Zeniuus <jsh56son@kaist.ac.kr>
2017-08-11 16:34:23 +0900

    VideoHorizontalActivity layout issue

--- app/src/main/java/com/zeniuus/www/reactiontagging/activities/VideoHorizontalActivity.java
+++ app/src/main/java/com/zeniuus/www/reactiontagging/activities/VideoHorizontalActivity.java
@@ -1,18 +1,34 @@
 package com.zeniuus.www.reactiontagging.activities;
 
+import android.app.Service;
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.zeniuus.www.reactiontagging.R;
+import com.zeniuus.www.reactiontagging.helpers.SoftKeyboard;
+import com.zeniuus.www.reactiontagging.managers.FeedbackManager;
 
 import org.w3c.dom.Text;
 
@@ -24,22 +40,61 @@ import static java.lang.Thread.sleep;
  */
 
 public class VideoHorizontalActivity extends AppCompatActivity {
+
+    TextView titleView;
     VideoView videoView;
+
+    LinearLayout progressLayout;
     ProgressBar progressBar;
+    ImageView playPauseIconView;
     TextView playTimeTextView;
+    EditText myFeedbackInputView;
+    Button myFeedbackSubmitBtn;
+
+    LinearLayout feedbackListLayout;
+    TextView feedbackListBtn;
+    ListView feedbackListView;
+
+    FeedbackListAdapter feedbackListAdapter;
+
+
+//    String videoName;
+//    String userId;
+
+//    SoftKeyboard softKeyboard;
+    FeedbackManager feedbackManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horizontal_video);
 
+//        videoName = getIntent().getStringExtra("video name");
+//        userId = getIntent().getStringExtra("userId");
+
+//        softKeyboard = new SoftKeyboard
+//                ((ViewGroup) findViewById(android.R.id.content), (InputMethodManager) getSystemService(Service.INPUT_METHOD_SERVICE));
+//        softKeyboard.setSoftKeyboardCallback(new SoftKeyboard.SoftKeyboardChanged()
+//        {
+//            @Override
+//            public void onSoftKeyboardHide()
+//            {
+//
+//            }
+//
+//            @Override
+//            public void onSoftKeyboardShow()
+//            {
+//
+//            }
+//        });
+//        feedbackManager = new FeedbackManager(videoName, userId, this);
+
+        titleView = (TextView) findViewById(R.id.title);
+//        titleView.setText(videoName);
+
         videoView = (VideoView) findViewById(R.id.video_view);
         videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.cat_behavior));
-
-        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-
-        playTimeTextView = (TextView) findViewById(R.id.play_time);
-
         videoView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -48,16 +103,134 @@ public class VideoHorizontalActivity extends AppCompatActivity {
                         && event.getY() >= 0
                         && event.getX() <= videoView.getWidth()
                         && event.getY() <= videoView.getHeight()) {
-                    if (videoView.isPlaying()) videoView.pause();
-                    else {
-                        videoView.start();
-                        new ProgressController().execute();
+//                    if (videoView.isPlaying()) videoPause();
+//                    else {
+//                        videoStart();
+//                        new ProgressController().execute();
+//                    }
+                    if (progressLayout.getVisibility() == View.GONE) {
+                        titleView.setVisibility(View.VISIBLE);
+                        progressLayout.setVisibility(View.VISIBLE);
+                    } else {
+                        titleView.setVisibility(View.GONE);
+                        progressLayout.setVisibility(View.GONE);
                     }
                 }
 
                 return true;
             }
         });
+
+        progressLayout = (LinearLayout) findViewById(R.id.progress_layout);
+        progressLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setProgress(0);
+        progressBar.setMax(100);
+        progressBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN
+                        || event.getAction() == MotionEvent.ACTION_MOVE) {
+                    videoPause();
+                    moveProgressBar(event);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    startVideo();
+//                    new ProgressController().execute();
+                }
+                return true;
+            }
+        });
+
+        playPauseIconView = (ImageView) findViewById(R.id.play_pause_icon);
+        playPauseIconView.setImageResource(R.drawable.ic_play);
+        playPauseIconView.setTag(R.drawable.ic_play);
+        playPauseIconView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((int) v.getTag() == R.drawable.ic_play) videoStart();
+                else videoPause();
+            }
+        });
+
+        playTimeTextView = (TextView) findViewById(R.id.play_time);
+
+        myFeedbackInputView = (EditText) findViewById(R.id.my_feedback_input);
+        myFeedbackInputView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP)
+                    videoPause();
+                return false;
+            }
+        });
+
+        myFeedbackSubmitBtn = (Button) findViewById(R.id.my_feedback_submit_btn);
+        myFeedbackSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("event", "give a new feedback");
+
+                int startTime = videoView.getCurrentPosition();
+
+                if (feedbackManager.addItem(
+                        Integer.toString(startTime),
+                        Integer.toString(startTime + 5000),
+                        myFeedbackInputView.getText().toString()) == -2)
+                    Toast.makeText(VideoHorizontalActivity.this, "Please give a richer feedback", Toast.LENGTH_SHORT).show();
+                else {
+                    myFeedbackInputView.setText("");
+//                    softKeyboard.closeSoftKeyboard();
+                }
+            }
+        });
+
+        feedbackListLayout = (LinearLayout) findViewById(R.id.feedback_list_layout);
+        feedbackListLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        feedbackListBtn = (TextView) findViewById(R.id.feedback_list_btn);
+        feedbackListBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (feedbackListLayout.getVisibility() == View.GONE) {
+                        feedbackListLayout.setVisibility(View.VISIBLE);
+                        feedbackListBtn.setText(">");
+                    } else {
+                        feedbackListLayout.setVisibility(View.GONE);
+                        feedbackListBtn.setText("<");
+                    }
+                }
+
+                return true;
+            }
+        });
+
+        feedbackListView = (ListView) findViewById(R.id.feedback_list);
+        feedbackListAdapter =
+    }
+
+    private void videoPause() {
+        playPauseIconView.setImageResource(R.drawable.ic_play);
+        playPauseIconView.setTag(R.drawable.ic_play);
+        videoView.pause();
+    }
+
+    private void videoStart() {
+        playPauseIconView.setImageResource(R.drawable.ic_pause);
+        playPauseIconView.setTag(R.drawable.ic_pause);
+        videoView.start();
+        new ProgressController().execute();
     }
 
     private class ProgressController extends AsyncTask<Void, Void, Void> {
@@ -115,6 +288,14 @@ public class VideoHorizontalActivity extends AppCompatActivity {
         videoView.seekTo((int)(videoView.getDuration() * (x / width)));
 //        updateFeedback(feedbackManager.getFeedbacksAtTime((int)(videoView.getDuration() * (x / width))));
         playTimeTextView.setText(milisecToMinSec((int)(videoView.getDuration() * (x / width))) + " / " + milisecToMinSec(videoView.getDuration()));
+    }
+
+    private class FeedbackListAdapter extends ArrayAdapter {
+        public FeedbackListAdapter(@NonNull Context context, @LayoutRes int resource) {
+            super(context, resource);
+        }
+
+
     }
 
 }
